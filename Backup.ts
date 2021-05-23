c263a7d2b3f0a6a2bda7b1d914be43e3676a913f
Liam Hughes <liam@liamhughes.com>
2021-05-23 14:03:03 +1000

    Backup script

--- Backup.ts
+++ Backup.ts
@@ -1,8 +1,11 @@
-import dotenv from 'dotenv'
-
+import dotenv from "dotenv";
 dotenv.config();
 
+import * as fs from "fs";
 import { Client, LogLevel } from "@notionhq/client";
+import { DatabasesQueryParameters } from "@notionhq/client/build/src/api-endpoints";
+
+const databaseID = "dae968ec2e6a4e15aec83a25c790b1a3";
 
 const main = async () => {
     const notion = new Client({
@@ -10,9 +13,48 @@ const main = async () => {
         logLevel: LogLevel.DEBUG,
     });
 
-    const tasksDatabase = await notion.databases.retrieve({"database_id": "dae968ec2e6a4e15aec83a25c790b1a3"});
+    const tasksDatabase = await notion.databases.retrieve({"database_id": databaseID});
 
     console.log(tasksDatabase);
+
+    fs.writeFileSync("Database.json", JSON.stringify(tasksDatabase, null, 2));
+
+    let hasMore = true;
+    let nextCursor : string | null = null;
+    const tasks = [];
+
+    while (hasMore){
+        const request : DatabasesQueryParameters = {
+            database_id: databaseID,
+            sorts: [
+                {
+                    property: "Category",
+                    direction: "ascending",
+                },
+                {
+                    property: "Priority",
+                    direction: "ascending",
+                },
+                {
+                    property: "Start Date",
+                    direction: "ascending",
+                }
+            ],
+        };
+
+        if (nextCursor !== null)
+            request.start_cursor = nextCursor;
+
+        const response = await notion.databases.query(request);
+
+        tasks.push(...response.results);
+        hasMore = response.has_more;
+        nextCursor = response.next_cursor;
+    }
+
+    console.log(tasks);
+
+    fs.writeFileSync("Tasks.json", JSON.stringify(tasks, null, 2));
 };
 
 main();
